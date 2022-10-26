6f6776fcd29d3155cbd62d9750d63556f2a3d586
obrunofontana <obrunofontana@gmail.com>
2022-10-26 11:50:47 -0300

    fea: adiciona cache nas queries do graphql

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,15 +1,64 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { createClient, Provider } from 'urql';
-
+import { createClient, dedupExchange, fetchExchange, Provider } from 'urql';
+import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache';
 
 import theme from '../theme';
 import { AppProps } from 'next/app';
+import { LoginMutation, MeDocument, MeQuery, RegisterMutation } from '../generated/graphql';
+
+// TODO: não entendi muito bem, revisar este trecho
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  queryInput: QueryInput,
+  result: any,
+  fn: (result: Result, query: Query) => Query
+) {
+  return cache.updateQuery(queryInput, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: 'http://localhost:4000/graphql',
   fetchOptions: {
     credentials: 'include'
-  }
+  },
+  exchanges: [dedupExchange, cacheExchange({
+    updates: {
+      Mutation: {
+        login: (_result, args, cache, info) => { 
+          betterUpdateQuery<LoginMutation, MeQuery>(
+            cache, 
+            { query: MeDocument},
+            _result,
+            (result, query) => {
+              if (result.login.errors) {
+                return query;
+              } else {
+                return {
+                  me: result.login.user
+                }
+              }
+            }
+          )
+        },
+        register: (_result, args, cache, info) => {   
+          betterUpdateQuery<RegisterMutation, MeQuery>(
+            cache, 
+            { query: MeDocument},
+            _result,
+            (result, query) => {
+              if (result.register.errors) {
+                return query;
+              } else {
+                return {
+                  me: result.register.user
+                }
+              }
+            }
+          )
+        }
+      }
+    }
+  }), fetchExchange]
 });
 
 
